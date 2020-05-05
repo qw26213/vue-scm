250a62b3d5e521babe168ebeae5d96b076ab4d88
wanghongguo <1158546221@qq.com>
2020-05-05 18:10:12 +0800

    rrrrrr

--- vue.config.js
+++ vue.config.js
@@ -77,8 +77,8 @@ module.exports = {
   devServer: {
     proxy: {
       '/drp': {
-        target: 'http://49.232.47.16/',
-        // target: 'http://192.168.1.107/',
+        // target: 'http://49.232.47.16/',
+        target: 'http://192.168.10.19/',
         ws: true,
         changeOrigin: true,// 如果接口跨域，需要进行这个参数配置
         // secure: false,// 如果是https接口，需要配置这个参数
