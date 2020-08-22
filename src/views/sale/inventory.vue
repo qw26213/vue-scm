4ae4328048c8ee3b5ef92233680e3e01189fe30a
wanghongguo <1158546221@qq.com>
2020-08-22 13:49:42 +0800

    bugfx

---
+++ src/views/sale/inventory.vue
@@ -0,0 +1,205 @@
+<template>
+    <div class="app-container">
+        <div class="filter-container">
+            <el-date-picker :editable="false" v-model="listQuery.queryParam.billDate1" type="date" placeholder="开始日期" size="mini" :clearable="false" value-format="yyyy-MM-dd"></el-date-picker>
+            <span class="zhi">至</span>
+            <el-date-picker :editable="false" v-model="listQuery.queryParam.billDate2" type="date" placeholder="结束日期" size="mini" :clearable="false" value-format="yyyy-MM-dd"></el-date-picker>
+            <staffList @selectChange="selectChange" ctrType="list"></staffList>
+            <el-select v-model="listQuery.queryParam.status" placeholder="单据状态" size="mini">
+                <el-option label="全部" :value="null"></el-option>
+                <el-option label="未审核" value="0"></el-option>
+                <el-option label="已审核" value="1"></el-option>
+            </el-select>
+            <el-button size="mini" type="primary" @click="getList">查询</el-button>
+            <el-button size="mini" type="primary" @click="handleAdd">新增</el-button>
+        </div>
+        <el-table :key="tableKey" v-loading="listLoading" :data="tableData" border fit highlight-current-row style="width: 100%;" size="mini">
+            <el-table-column label="序号" type="index" width="50" align="center" />
+            <el-table-column label="单据日期" align="center" width="100">
+                <template slot-scope="{row}">
+                    <span>{{row.billDate}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="单据号" width="100">
+                <template slot-scope="{row}">
+                    <span>{{row.billNo}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="销售单数" align="center">
+                <template slot-scope="{row}">
+                    <span>{{row.soBillNum}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="开始日期" align="center">
+                <template slot-scope="{row}">
+                    <span>{{row.beginDate}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="结束日期" align="center">
+                <template slot-scope="{row}">
+                    <span>{{row.endDate}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="开始余额" align="right">
+                <template slot-scope="{row}">
+                    <span>{{row.beginBalance|Fixed}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="交款金额" align="right">
+                <template slot-scope="{row}">
+                    <span>{{row.settleCurrAmount|Fixed}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="余额" align="right">
+                <template slot-scope="{row}">
+                    <span>{{row.balance|Fixed}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="现金合计" align="right">
+                <template slot-scope="{row}">
+                    <span>{{row.sumCurrAmount|Fixed}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="状态" align="center">
+                <template slot-scope="{row}">
+                    <span>{{row.status==1?'已审核':row.status==2?'已生成':'待审核'}}</span>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" align="center" width="200">
+                <template slot-scope="{row}">
+                    <span class="ctrl" v-if="row.status==0" @click="handleCompile(row.id)">编辑</span>
+                    <span class="ctrl" v-if="row.status==1" @click="handleScan(row.id)">查看</span>
+                    <span class="ctrl" v-if="row.status==0" @click="handleCheck(row.id)">审核</span>
+                    <span class="ctrl" v-if="row.status==1" @click="handleConfirm(row.id)">确认</span>
+                    <span class="ctrl del" v-if="row.status==0" @click="handleDel(row.id)">删除</span>
+                </template>
+            </el-table-column>
+        </el-table>
+        <pagination v-show="total>20" :total="total" :page.sync="listQuery.pageIndex" :limit.sync="listQuery.pageNum" @pagination="getList" />
+    </div>
+</template>
+<script>
+import { getEverydayTotal, delEverydayTotal, auditEverydayTotal, confirmEverydayTotal } from '@/api/sale'
+import Pagination from '@/components/Pagination'
+import staffList from '@/components/selects/staffList';
+import { getNowDate } from '@/utils/auth'
+export default {
+    name: 'EverydayTotalData',
+    components: { Pagination, staffList },
+    data() {
+        return {
+            tableKey: 0,
+            tableData: [],
+            total: 0,
+            isBillDate: '0',
+            dialogFormVisible: false,
+            listLoading: true,
+            curBillId: '',
+            listQuery: {
+                pageIndex: 1,
+                pageNum: 20,
+                queryParam: {
+                    billDate1: getNowDate(),
+                    billDate2: getNowDate(),
+                    billNo: "",
+                    staffId: '',
+                    custId: '',
+                    status: '',
+                    isJeHeader: '',
+                    nonZeroBalance: 0
+                }
+            }
+        }
+    },
+    filters: {
+        Fixed: function (num) {
+            if (!num) { return '0.00' }
+            return parseFloat(num).toFixed(2);
+        }
+    },
+    created() {
+        this.getList();
+    },
+    methods: {
+        getList() {
+            this.listLoading = true
+            getEverydayTotal(this.listQuery).then(res => {
+                this.listLoading = false
+                this.tableData = res.data.data
+                this.total = res.data.totalNum
+            }).catch(err => {
+                this.listLoading = false
+            })
+        },
+        selectChange(obj) {
+            for (var key in obj) {
+                this.listQuery.queryParam[key] = obj[key];
+            }
+        },
+        handleCheck(id) {
+            this.$confirm('确定审核通过吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                auditEverydayTotal(id).then(res => {
+                    if (res.data.errorCode == 0) {
+                        this.getList();
+                        this.$message.success('审核成功')
+                    } else {
+                        this.$message.error(res.data.msg)
+                    }
+                })
+            }).catch(() => {
+                console.log('取消')
+            });
+        },
+        handleAdd(obj) {
+            this.$store.dispatch('tagsView/delView', this.$route)
+            this.$router.push('/sale/everydayAdd')
+        },
+        handleCompile(id) {
+            this.$store.dispatch('tagsView/delView', this.$route)
+            this.$router.push('/sale/everydayModify?id=' + id)
+        },
+        handleConfirm(id) {
+            this.$confirm('确定要确认吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                confirmEverydayTotal(id).then(res => {
+                    if (res.data.errorCode == 0) {
+                        this.getList();
+                        this.$message.success('确认成功')
+                    } else {
+                        this.$message.error(res.data.msg)
+                    }
+                })
+            }).catch(() => {
+                console.log('取消')
+            });
+        },
+        handleDel(id) {
+            this.$confirm('确定删除吗?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).then(() => {
+                this.delItem(id)
+            })
+        },
+        delItem(id) {
+            delEverydayTotal(id).then(res => {
+                if (res.data.errorCode == 0) {
+                    this.getList();
+                    this.dialogFormVisible = false
+                    this.$message.success('删除成功')
+                } else {
+                    this.$message.error(res.data.msg)
+                }
+            })
+        }
+    }
+}
+</script>
---
+++ src/views/sale/inventoryAdd.vue
@@ -0,0 +1,208 @@
+everyday<template>
+    <div class="app-container">
+        <div class="dataTable">
+            <el-form :inline="true" label-position="left" label-width="120px" style="width:750px; margin:10px auto;">
+                <el-form-item label="单据日期:" prop="billDate">
+                    <el-date-picker :editable="false" class="input-item" v-model="temp.billDate" type="date" placeholder="单据日期" size="small" :clearable="false" value-format="yyyy-MM-dd" disabled />
+                </el-form-item>
+                <el-form-item label="单据号:" prop="billNo">
+                    <el-input size="small" class="input-item" v-model="temp.billNo" placeholder="单据号" disabled />
+                </el-form-item>
+                <el-form-item label="开始日期:" prop="billDate">
+                    <el-date-picker :editable="false" class="input-item" v-model="temp.beginDate" type="date" placeholder="单据日期" size="small" :picker-options="pickerOptions" :clearable="false" value-format="yyyy-MM-dd" />
+                </el-form-item>
+                <el-form-item label="结束日期:" prop="billDate">
+                    <el-date-picker :editable="false" class="input-item" v-model="temp.endDate" type="date" placeholder="单据日期" size="small" :picker-options="pickerOptions" :clearable="false" value-format="yyyy-MM-dd" />
+                </el-form-item>
+                <el-form-item label="业务员:" prop="staffId">
+                    <el-select v-model="temp.staffId" style="width:240px" disabled placeholder="选择业务员" size="small">
+                        <el-option v-for="item in stafflist" :label="item.staffName" :value="item.id"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="销售单数:" prop="soBillNum">
+                    <el-input size="small" class="input-item" v-model="temp.soBillNum" placeholder="销售单数" disabled />
+                </el-form-item>
+                <el-form-item label="销售退款单数:" prop="soReturnedBillNum">
+                    <el-input size="small" class="input-item" v-model="temp.soReturnedBillNum" placeholder="销售单数" disabled />
+                </el-form-item>
+                <el-form-item label="预收单数:" prop="psBillNum">
+                    <el-input size="small" class="input-item" v-model="temp.psBillNum" placeholder="预收单数" disabled />
+                </el-form-item>
+                <el-form-item label="预收退款单数:" prop="psReturnedBillNum">
+                    <el-input size="small" class="input-item" v-model="temp.psReturnedBillNum" placeholder="预收退款单数" disabled />
+                </el-form-item>
+                <el-form-item label="收款单数:" prop="receiptBillNum">
+                    <el-input size="small" class="input-item" v-model="temp.receiptBillNum" placeholder="收款单数" disabled />
+                </el-form-item>
+                <el-form-item label="付款单数:" prop="paymentNum">
+                    <el-input size="small" class="input-item" v-model="temp.paymentNum" placeholder="付款单数" disabled />
+                </el-form-item>
+                <el-form-item label="销售现金金额:" prop="soCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.soCurrAmount" placeholder="销售现金金额" disabled />
+                </el-form-item>
+                <el-form-item label="销售退款现金金额:" prop="soReturnedCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.soReturnedCurrAmount" placeholder="销售退款现金金额" disabled />
+                </el-form-item>
+                <el-form-item label="预收款现金金额:" prop="psCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.psCurrAmount" placeholder="预收款现金金额" disabled />
+                </el-form-item>
+                <el-form-item label="预收款退款现金金额:" prop="psReturnedCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.psReturnedCurrAmount" placeholder="预收款退款现金金额" disabled />
+                </el-form-item>
+                <el-form-item label="收款现金金额:" prop="receiptCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.receiptCurrAmount" placeholder="收款现金金额" disabled />
+                </el-form-item>
+                <el-form-item label="付款现金金额:" prop="paymentCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.paymentCurrAmount" placeholder="付款现金金额" disabled />
+                </el-form-item>
+                <el-form-item label="现金合计:" prop="sumCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.sumCurrAmount" placeholder="现金合计" disabled />
+                </el-form-item>
+                <el-form-item label="开始余额:" prop="beginBalance">
+                    <el-input size="small" class="input-item" v-model="temp.beginBalance" placeholder="开始余额" disabled />
+                </el-form-item>
+                <el-form-item label="交款金额:" prop="settleCurrAmount">
+                    <el-input size="small" class="input-item" v-model="temp.settleCurrAmount" placeholder="交款金额" />
+                </el-form-item>
+                <el-form-item label="余额:" prop="balance">
+                    <el-input size="small" class="input-item" v-model="temp.balance" placeholder="余额" disabled />
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="tx-c" style="margin-top:15px">
+            <el-button class="filter-item" type="primary" @click="save">保存</el-button>
+        </div>
+        <el-dialog :close-on-click-modal="false" title="选择截止日期" :visible.sync="dialogFormVisible" :show-close="false" width="420px">
+            <el-form ref="dataForm" :rules="rules" :model="form" label-position="left" label-width="88px" style="width:400px; margin:10px 25px;">
+                <el-form-item label="业务员:" prop="staffId">
+                    <el-select v-model="form.staffId" style="width:240px" placeholder="选择业务员" size="small">
+                        <el-option v-for="item in stafflist" :label="item.staffName" :value="item.id"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="截止日期:" prop="endDate">
+                    <el-date-picker :editable="false" class="input-item" v-model="form.endDate" type="date" placeholder="截止日期" size="small" :clearable="false" :picker-options="pickerOptions" value-format="yyyy-MM-dd" />
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer" align="center">
+                <el-button type="primary" @click="initEveryday">确定</el-button>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+<script>
+import { saveEverydayTotal, queryDataByStaffId, getEverydayTotalById } from '@/api/sale'
+import { getStaff } from '@/api/basedata'
+import { deleteEmptyProp, addNullObj, addNullObj2 } from '@/utils'
+import staffList from '@/components/selects/staffList'
+import { getName, getNowDate } from '@/utils/auth'
+export default {
+    name: 'everydayAdd',
+    components:{ staffList },
+    data() {
+        return {
+            id:'',
+            isDisabled:false,
+            status:this.$route.query.status,
+            invCatgList:[],
+            stafflist:[],
+            temp: {
+                billNo: '',
+                staffId: '',
+                beginDate: '',
+                endDate: '',
+                beginBalance: '',
+                auditDate: "",
+                auditor: "",
+                balance: 0
+            },
+            form: {
+                staffId: '',
+                endDate: ''
+            },
+            dialogFormVisible:false,
+            settleData:[{},{},{},{},{}],
+            rules: {
+                staffId: [{ required: true, message: '业务员不能为空', trigger: 'change' }],
+                endDate: [{ required: true, message: '截止日期不能为空', trigger: 'change' }]
+            },
+            pickerOptions: {
+              disabledDate(time) {
+                return time.getTime() > Date.now();
+              },
+              shortcuts: [{
+                text: '今天',
+                onClick(picker) {
+                  picker.$emit('pick', new Date());
+                }
+              }, {
+                text: '昨天',
+                onClick(picker) {
+                  const date = new Date();
+                  date.setTime(date.getTime() - 3600 * 1000 * 24);
+                  picker.$emit('pick', date);
+                }
+              }, {
+                text: '一周前',
+                onClick(picker) {
+                  const date = new Date();
+                  date.setTime(date.getTime() - 3600 * 1000 * 24 * 7);
+                  picker.$emit('pick', date);
+                }
+              }]
+            }
+        }
+    },
+    created() {
+        getStaff().then(res => {
+            this.stafflist = res.data.data || []
+        })
+        if(this.$route.query.id){
+            this.id = this.$route.query.id;
+            getEverydayTotalById(this.id).then(res=>{
+                for(var key in this.temp){
+                    this.temp[key] = res.data.data[key]
+                }
+            })
+        } else {
+            this.showModal()
+        }
+    },
+    beforeDestroy() {
+        this.dialogFormVisible = false
+    },
+    methods: {
+        showModal() {
+            this.dialogFormVisible = true
+        },
+        initEveryday() {
+            this.$refs['dataForm'].validate((valid) => {
+                if (valid) {
+                    const data = this.form
+                    queryDataByStaffId(data).then(res => {
+                        this.temp = res.data.data
+                        this.dialogFormVisible = false
+                    })
+                }
+            })
+        },
+        save() {
+            this.temp.id = this.id
+            saveEverydayTotal(this.temp).then(res => {
+                if (res.data.errorCode == 0) {
+                    this.$message.success(this.temp.id==""?'新增成功':'修改成功');
+                    this.$store.dispatch('tagsView/delView', this.$route);
+                    this.$router.replace('/sale/everyday');
+                } else {
+                    this.$message.error(res.data.msg)
+                }
+            }).catch(()=>{
+                this.$message.error('保存失败，请稍后重试！')
+            })
+        }
+    }
+}
+</script>
+<style scoped>
+.input-item{width: 240px;}
+</style>
+
