13750bcf99be1b76a54e1db75dd5ddb6619c699c
wanghongguo <1158546221@qq.com>
2020-09-15 20:59:57 +0800

    ttttt

--- src/router/modules/reportRouter.js
+++ src/router/modules/reportRouter.js
@@ -6,80 +6,90 @@ const reportRouter = {
     redirect: '/table/data',
     name: 'table',
     meta: {
-        roles: ['6010', '6020', '6030'],
+        roles: ['5010', '5020', '5030', '5036', '5040', '5050', '5060'],
         title: '报表',
         icon: 'table'
     },
     children: [{
+        path: 'purchase',
+        component: setView,
+        name: 'purchase12',
+        meta: {
+            roles: ['5010'],
+            title: '采购'
+        },
+        children: [{
+            path: 'purchase1',
+            component: () => import ('@/views/table/purchase/data1'),
+            name: 'purchase1',
+            meta: {
+                roles: ['501010'],
+                title: '采购统计'
+            }
+        }, {
+            path: 'purchase2',
+            component: () => import ('@/views/table/purchase/data2'),
+            name: 'purchase2',
+            meta: {
+                roles: ['501020'],
+                title: '采购退货统计'
+            }
+        }]
+    }, {
+        path: 'sale',
+        component: setView,
+        name: 'sale1',
+        meta: {
+            roles: ['5020'],
+            title: '销售'
+        },
+        children: [{
+            path: 'graph',
+            component: () => import ('@/views/table/sale/data1'),
+            name: 'graph1',
+            meta: {
+                roles: ['502010'],
+                title: '销售统计'
+            }
+        }, {
+            path: 'data',
+            component: () => import ('@/views/table/sale/data2'),
+            name: 'graph2',
+            meta: {
+                roles: ['502020'],
+                title: '销售退货统计'
+            }
+        }]
+    }, {
         path: 'store',
         component: setView,
         name: 'tableStore',
         meta: {
-            roles: ['6010'],
+            roles: ['5030'],
             title: '库存'
         },
         children: [{
-            path: 'warehouse',
-            component: () => import ('@/views/table/store/warehouse'),
-            name: 'warehouse1',
+            path: 'warehousing1',
+            component: () => import ('@/views/table/store/warehousing1'),
+            name: 'warehousing1',
             meta: {
-                roles: ['601018'],
-                title: '仓库统计'
-            }
-        }, {
-            path: 'truck',
-            component: () => import ('@/views/table/store/truck'),
-            name: 'truck1',
-            meta: {
-                roles: ['601018'],
-                title: '车辆统计'
+                roles: ['503010'],
+                title: '入库统计'
             }
         }, {
             path: 'outbound1',
             component: () => import ('@/views/table/store/outboundOrder1'),
             name: 'outbound1',
             meta: {
-                roles: ['601018'],
-                title: '销售出库统计'
-            }
-        }, {
-            path: 'outbound2',
-            component: () => import ('@/views/table/store/outboundOrder2'),
-            name: 'outbound2',
-            meta: {
-                roles: ['601018'],
-                title: '销售退库统计'
-            }
-        }, {
-            path: 'warehousing1',
-            component: () => import ('@/views/table/store/warehousing1'),
-            name: 'warehousing1',
-            meta: {
-                roles: ['601018'],
-                title: '入库统计'
-            }
-        }, {
-            path: 'warehousing2',
-            component: () => import ('@/views/table/store/warehousing2'),
-            name: 'warehousing2',
-            meta: {
-                roles: ['601018'],
-                title: '退货出库统计'
-            }
-        }, {
-            path: 'inventory',
-            component: () => import ('@/views/table/store/inventory'),
-            name: 'inventory1',
-            meta: {
-                roles: ['6030'],
-                title: '盘点统计'
+                roles: ['503020'],
+                title: '出库统计'
             }
         }, {
             path: 'allocation',
             component: () => import ('@/views/table/store/allocation'),
             name: 'allocation1',
             meta: {
-                roles: ['601018'],
+                roles: ['503030'],
                 title: '调拨统计'
             }
         }, {
@@ -87,8 +97,81 @@ const reportRouter = {
             component: () => import ('@/views/table/store/movement'),
             name: 'movement',
             meta: {
-                roles: ['601018'],
+                roles: ['503040'],
                 title: '移库统计'
+            }
+        }, {
+            path: 'outbound2',
+            component: () => import ('@/views/table/store/outboundOrder2'),
+            name: 'outbound2',
+            meta: {
+                roles: ['503050'],
+                title: '销售退货入库'
+            }
+        }, {
+            path: 'warehousing2',
+            component: () => import ('@/views/table/store/warehousing2'),
+            name: 'warehousing2',
+            meta: {
+                roles: ['503060'],
+                title: '采购退货出库'
+            }
+        }, {
+            path: 'inventory',
+            component: () => import ('@/views/table/store/inventory'),
+            name: 'inventory1',
+            meta: {
+                roles: ['503070'],
+                title: '盘点统计'
+            }
+        }, {
+            path: 'warehouse',
+            component: () => import ('@/views/table/store/warehouse'),
+            name: 'warehouse1',
+            meta: {
+                roles: ['503080'],
+                title: '仓库统计'
+            }
+        }, {
+            path: 'truck',
+            component: () => import ('@/views/table/store/truck'),
+            name: 'truck1',
+            meta: {
+                roles: ['503090'],
+                title: '车辆统计'
+            }
+        }]
+    }, {
+        path: 'presale',
+        component: () => import ('@/views/table/presale/data'),
+        name: 'presale1',
+        meta: {
+            roles: ['5036'],
+            title: '预收'
+        }
+    }, {
+        path: 'rpt',
+        component: setView,
+        name: 'rpt1',
+        meta: {
+            roles: ['5030'],
+            title: '往来'
+        },
+        children: [{
+            path: 'rpt1',
+            component: () => import ('@/views/table/rpt/data1'),
+            name: 'rpt1',
+            meta: {
+                roles: ['503010'],
+                title: '收款'
+            }
+        }, {
+            path: 'rpt2',
+            component: () => import ('@/views/table/rpt/data2'),
+            name: 'rpt2',
+            meta: {
+                roles: ['503020'],
+                title: '付款'
             }
         }]
     }, {
@@ -97,7 +180,7 @@ const reportRouter = {
         redirect: '/grossprofit/group',
         name: 'grossprofit',
         meta: {
-            roles: ['5410', '5420', '5430', '5440', '5450', '5460'],
+            roles: ['5050'],
             title: '毛利'
         },
         children: [{
@@ -105,7 +188,7 @@ const reportRouter = {
             component: () => import ('@/views/table/grossprofit/grossprofit'),
             name: 'grossprofit',
             meta: {
-                roles: ['5460'],
+                roles: ['505010'],
                 title: '综合统计'
             }
         }, {
@@ -113,7 +196,7 @@ const reportRouter = {
             component: () => import ('@/views/table/grossprofit/grossprofit3'),
             name: 'grossprofit3',
             meta: {
-                roles: ['5430'],
+                roles: ['505020'],
                 title: '周期辅助项统计'
             }
         }, {
@@ -121,7 +204,7 @@ const reportRouter = {
             component: () => import ('@/views/table/grossprofit/grossprofit4'),
             name: 'grossprofit4',
             meta: {
-                roles: ['5440'],
+                roles: ['505030'],
                 title: '辅助项二维统计'
             }
         }, {
@@ -129,91 +212,8 @@ const reportRouter = {
             component: () => import ('@/views/table/grossprofit/grossprofit5'),
             name: 'grossprofit5',
             meta: {
-                roles: ['5450'],
+                roles: ['505040'],
                 title: '品类分项统计'
-            }
-        }]
-    }, {
-        path: 'purchase',
-        component: setView,
-        name: 'purchase12',
-        meta: {
-            roles: ['6020'],
-            title: '采购'
-        },
-        children: [{
-            path: 'purchase1',
-            component: () => import ('@/views/table/purchase/data1'),
-            name: 'purchase1',
-            meta: {
-                roles: ['602010'],
-                title: '采购统计'
-            }
-        }, {
-            path: 'purchase2',
-            component: () => import ('@/views/table/purchase/data2'),
-            name: 'purchase2',
-            meta: {
-                roles: ['602010'],
-                title: '采购退货统计'
-            }
-        }]
-    }, {
-        path: 'presale',
-        component: () => import ('@/views/table/presale/data'),
-        name: 'presale1',
-        meta: {
-            roles: ['6020'],
-            title: '预收'
-        }
-    }, {
-        path: 'sale',
-        component: setView,
-        name: 'sale1',
-        meta: {
-            roles: ['6030'],
-            title: '销售'
-        },
-        children: [{
-            path: 'graph',
-            component: () => import ('@/views/table/sale/data1'),
-            name: 'graph1',
-            meta: {
-                roles: ['603010'],
-                title: '销售统计'
-            }
-        }, {
-            path: 'data',
-            component: () => import ('@/views/table/sale/data2'),
-            name: 'graph2',
-            meta: {
-                roles: ['603020'],
-                title: '销售退货统计'
-            }
-        }]
-    }, {
-        path: 'rpt',
-        component: setView,
-        name: 'rpt1',
-        meta: {
-            roles: ['6030'],
-            title: '往来'
-        },
-        children: [{
-            path: 'rpt1',
-            component: () => import ('@/views/table/rpt/data1'),
-            name: 'rpt1',
-            meta: {
-                roles: ['603010'],
-                title: '收款'
-            }
-        }, {
-            path: 'rpt2',
-            component: () => import ('@/views/table/rpt/data2'),
-            name: 'rpt2',
-            meta: {
-                roles: ['603020'],
-                title: '付款'
             }
         }]
     }, {
@@ -221,7 +221,7 @@ const reportRouter = {
         component: () => import ('@/views/table/visit/data'),
         name: 'visit1',
         meta: {
-            roles: ['6020'],
+            roles: ['5060'],
             title: '拜访'
         }
     }]
--- src/router/modules/rpRouter.js
+++ src/router/modules/rpRouter.js
@@ -61,55 +61,6 @@ const arapRouter = {
             roles: ['182020'],
             title: '修改付款单'
         }
-        // }, {
-        //     path: 'rpData2',
-        //     component: () => import ('@/views/rp/data2'),
-        //     name: 'rpData2',
-        //     meta: {roles: ['181010'],
-        //         title: '其它应收'
-        //     }
-        // }, {
-        //     path: 'rpData3',
-        //     component: () => import ('@/views/rp/data3'),
-        //     name: 'rpData3',
-        //     meta: {roles: ['181010'],
-        //         title: '其它应付'
-        //     }
-        // }, {
-        //     path: 'rpData5',
-        //     component: () => import ('@/views/rp/data5'),
-        //     name: 'rpData5',
-        //     meta: {roles: ['181010'],
-        //         title: '收入单'
-        //     }
-        // }, {
-        //     path: 'rpData6',
-        //     component: () => import ('@/views/rp/data6'),
-        //     name: 'rpData6',
-        //     meta: {roles: ['181010'],
-        //         title: '费用单'
-        //     }
-        // }, {
-        //     path: 'rpData7',
-        //     component: () => import ('@/views/rp/data7'),
-        //     name: 'rpData7',
-        //     meta: {roles: ['181010'],
-        //         title: '往来核销'
-        //     }
-        // }, {
-        //     path: 'rpData8',
-        //     component: () => import ('@/views/rp/data8'),
-        //     name: 'rpData8',
-        //     meta: {roles: ['181010'],
-        //         title: '往来冲销'
-        //     }
-        // }, {
-        //     path: 'rpData9',
-        //     component: () => import ('@/views/rp/data9'),
-        //     name: 'rpData9',
-        //     meta: {roles: ['181010'],
-        //         title: '往来账'
-        //     }
     }]
 }
 export default arapRouter
